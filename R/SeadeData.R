df79108cdd3ac316e0ce1f5482452ff7e6e8a634
rdurl0 <durlo.raul@gmail.com>
2018-08-02 17:38:15 -0300

    include seade data

---
+++ R/SeadeData.R
@@ -0,0 +1,34 @@
+#' Population count in Sao Paulo state (1980 - 2018).
+#'
+#' A dataset containing resident population in Sao Paulo state by city (645) sex and age. The data was collected in 
+#' Fundacao SEADE's website.
+#'
+#' @format A \code{tibble} with 25.155 rows and 39 variables:
+#' \describe{
+#'   \item{Localidades (\code{municipio})}{cities in Sao Paulo}
+#'   \item{Periodos (\code{ano})}{year (1980 - 2018)}
+#'   \item{Populacao (\code{pop})}{Total opulation count}
+#'   \item{Populacao Feminina (+ages, \code{pop_fem})}{Female population count}
+#'   \item{Populacao Masculina (+ages, \code{pop_msc})}{Male population count}
+#'   \item{Cod. IBGE (\code{cd_geocmu})}{Geo code}
+#' }
+#' @source \url{https://www.seade.gov.br/}
+"seade_pop"
+
+
+#' VARIABLE NAMES: Population count in Sao Paulo state (1980 - 2018).
+#'
+#' A dataset containing resident population in Sao Paulo state by city (645) sex and age. The data was collected in 
+#' Fundacao SEADE's website.
+#'
+#' @format A \code{tibble} with 25.155 rows and 39 variables:
+#' \describe{
+#'   \item{Localidades (\code{municipio})}{cities in Sao Paulo}
+#'   \item{Periodos (\code{ano})}{year (1980 - 2018)}
+#'   \item{Populacao (\code{pop})}{Total opulation count}
+#'   \item{Populacao Feminina (+ages, \code{pop_fem})}{Female population count}
+#'   \item{Populacao Masculina (+ages, \code{pop_msc})}{Male population count}
+#'   \item{Cod. IBGE (\code{cd_geocmu})}{Geo code}
+#' }
+#' @source \url{https://www.seade.gov.br/}
+"variables_names_seade_pop"
